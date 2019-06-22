39b79c9c67c1f919088f0dd59dee75e85b6c71e9
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-06-22 20:54:36 +0800

    Create 手套.cc

---
+++ Question/手套.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int FindMinimum(int n, vector<int> left, vector<int> right) {
+	int ret = 0;
+	int left_min = INT_MAX;
+	int	right_min = INT_MAX;
+	int left_sum = 0;
+	int right_sum = 0;
+	for (int i = 0; i < n; i++){
+		//如果该颜色的手套只有左手或右手，则必须取,以保证覆盖每一种颜色
+		if (left[i] * right[i] == 0){
+			ret += left[i];
+			ret += right[i];
+		}
+		else{
+			//分别计算成对的手套左右的数量和
+			right_sum += right[i];
+			left_sum += left[i];
+			//分别记录左右的最小数量
+			right_min = min(right_min, right[i]);
+			left_min = min(left_min, left[i]);
+		}
+	}
+	//right_ret = 右边数量和 -右边的最小值 + 1
+	//left_ret = 左边数量和 - 左边最小值 + 1
+	int right_ret = right_sum - right_min + 1;
+	int left_ret = left_sum - left_min + 1;
+	//取左右结果的最小值 + 1，就可以保证了
+	ret += min(right_ret, left_ret) + 1;
+	return ret;
+}
+
+int main(){
+	int n;
+	vector<int> left;
+	vector<int> right;
+	while (cin >> n){
+		left.resize(n);
+		right.resize(n);
+		//输入
+		for (int i = 0; i < n; i++){
+			cin >> left[i];
+		}
+		for (int i = 0; i < n; i++){
+			cin >> right[i];
+		}
+		//输出
+		int ret = FindMinimum(n, left, right);
+		cout << ret << endl;
+	}
+	return 0;
+}
