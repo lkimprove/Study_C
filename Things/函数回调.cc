881dea23f1d9667de4e6fee7b8eda8fda658c32f
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-10-13 23:33:46 +0800

    Update 函数回调.cc

--- Things/函数回调.cc
+++ Things/函数回调.cc
@@ -4,3 +4,35 @@
 //超市进货 --- 触发回调函数的事件
 //店员给小明打电话 --- 回调函数调用
 //小明去超市将货物买下来 --- 响应回调事件
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+//回调函数
+void GetArray(vector<int>& _array, int count, int(*Get_RandNumber)()){
+	for (int i = 0; i < count; i++){
+		_array[i] = Get_RandNumber();
+	}
+}
+
+//获取随机值
+int Get_RandNumber(){
+	int ret = rand() % 100 ;
+	return ret;
+}
+
+
+int main(){
+	srand(time(NULL));
+	vector<int> _array(10, 0);
+	GetArray(_array, 10, Get_RandNumber);
+	
+	for (auto e : _array){
+		cout << e << " ";
+	}
+
+	cout << endl;
+	return 0;
+}
