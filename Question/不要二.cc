31501f8ed71c71da268e4037694c10ec2851e1b3
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-11-12 23:36:21 +0800

    Update 不要二.cc

--- Question/不要二.cc
+++ Question/不要二.cc
@@ -2,54 +2,53 @@
 #include <vector>
 using namespace std;
 
-int Ret(int H, int W){
-	// 初始化一个 H 行 W 列的二维数组
-	vector<vector<int>> vv;
-	vv.resize(H);
-	for (int i = 0; i < H; i++){
-		//每一个位置都填 1 表示有蛋糕
-		vv[i].resize(W, 1);
-	}
-	//遍历这个二维数组
-	for (int i = 0; i < H; i++){
-		for (int j = 0; j < W; j++){
-			//将每一个不符合的位置的蛋糕取出
-			//两个蛋糕的欧几里得距离不等于2  ==> 行距离为 2 或 列距离为 2 的位置没有蛋糕
-			if (vv[i][j] == 1){
-				//防止越界
-				if (i - 2 > 0){
-					vv[i - 2][j] = 0;
-				}
-				if (i + 2 < H){
-					vv[i + 2][j] = 0;
-				}
-				if (j - 2 > 0){
-					vv[i][j - 2] = 0;
-				}
-				if (j + 2 < W){
-					vv[i][j + 2] = 0;
-				}
-			}
-		}
-	}
-	int ret = 0;
-	for (int i = 0; i < H; i++){
-		for (int j = 0; j < W; j++){
-			if (vv[i][j] == 1){
-				ret++;
-			}
-		}
-	}
-	return ret;
+static int Point[4][2] = {{0, 2}, {0, -2}, {2, 0}, {-2, 0}};
+
+void Box(vector<vector<int>>& vv, int H, int M, int x, int y){
+    //判断该位置是否有蛋糕
+    //若有蛋糕，将与其欧几里得距离为2的位置的蛋糕拿走
+    if(vv[x][y] != 1){
+        return;
+    }
+    
+    for(int i = 0; i < 4; i++){
+        int newx = x + Point[i][0];
+        int newy = y + Point[i][1];
+        
+        //防越界
+        if(newx >= 0 && newy >= 0 && newx < H && newy < M){
+            if(vv[newx][newy] == 1){
+                vv[newx][newy] = 0;
+            }
+        }
+    }
 }
 
+
 int main(){
-	int H, W;
-	//输入
-	while (cin >> H >> W){
-		int ret = Ret(H, W);
-		//输出
-		cout << ret << endl;
-	}
-	return 0;
+    int M, H;
+    while(cin >> M >> H){
+        vector<vector<int>> vv(H, vector<int>(M, 1));
+        
+        
+        for(int i = 0; i < H; i++){
+            for(int j = 0; j < M; j++){
+                Box(vv, H, M, i, j);
+            }
+        }
+        
+        //判断蛋糕个数
+        int ret = 0;
+        for(int i = 0; i < H; i++){
+            for(int j = 0; j < M; j++){
+                if(vv[i][j] == 1){
+                    ret++;
+                }
+            }
+        }
+        
+        cout << ret << endl;
+    }
+    
+    return 0;
 }
