7c0bad74ad0d8ed3e863bb2b45d6b17e5a9f9c5a
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-11-15 23:56:38 +0800

    Update 合法括号序列判断.cc

--- Question/合法括号序列判断.cc
+++ Question/合法括号序列判断.cc
@@ -26,3 +26,34 @@ public:
         
     }
 };
+
+
+//STL栈（stack）
+class Parenthesis {
+public:
+    bool chkParenthesis(string A, int n) {
+        // write code here
+        stack<char> s;
+        for(auto& it : A){
+            //入栈
+            if(it == '('){
+                s.push(it);
+            }
+            else if(it == ')'){
+                //判断是否栈中存在(
+                if(s.empty() || s.top() != '('){
+                    return false;
+                }
+                
+                //出栈
+                s.pop();
+            }
+            //字符非法
+            else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
