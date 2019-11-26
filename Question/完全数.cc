29093003548d05a81d478d713683d00c1f3b9225
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-11-26 17:04:28 +0800

    Update 完全数.cc

--- Question/完全数.cc
+++ Question/完全数.cc
@@ -7,12 +7,14 @@ int main(){
         int ret = 0;
         for(int i = 1; i <= n; i++){
             int sum = 0;
+            //遍历约数
             for(int j = 1; j < i; j++){
                 if(i % j == 0){
                     sum += j;
                 }
             }
             
+            //判断是否为完全数
             if(sum == i){
                 ret++;
             }
