13866e0484f9c5283bc0b1f60b8adbffb9bb022e
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-11-29 19:54:22 +0800

    Update 完全数.cc

--- Question/完全数.cc
+++ Question/完全数.cc
@@ -1,26 +1,30 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main(){
     int n;
     while(cin >> n){
         int ret = 0;
-        for(int i = 1; i <= n; i++){
-            int sum = 0;
-            //遍历约数
-            for(int j = 1; j < i; j++){
+        for(int i = 2; i <= n; i++){
+            int sum = 1;
+            
+            //求约数
+            for(int j = 2; j <= sqrt(i); j++){
                 if(i % j == 0){
                     sum += j;
+                    if(j != sqrt(i)){
+                        sum += (i / j);
+                    }
                 }
             }
             
-            //判断是否为完全数
             if(sum == i){
                 ret++;
             }
         }
-        
         cout << ret << endl;
+        
     }
     
     return 0;
