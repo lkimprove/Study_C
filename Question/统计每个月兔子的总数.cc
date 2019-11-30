8ccf2fd06046b4a032a84b24467a4aa3e5f89bee
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-11-30 19:54:09 +0800

    Update 统计每个月兔子的总数.cc

--- Question/统计每个月兔子的总数.cc
+++ Question/统计每个月兔子的总数.cc
@@ -17,3 +17,28 @@ int main(){
     
     return 0;
 }
+
+
+//修改版
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int month;
+    while(cin >> month){
+        int first = 1;
+        int second = 1;
+        int ret = 1;
+        
+        for(int i = 3; i <= month; i++){
+            ret = first + second;
+            first = second;
+            second = ret;
+        }
+        
+        cout << ret << endl;
+    }
+    
+    return 0;
+}
