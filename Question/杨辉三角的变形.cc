8a785db656d8717f543c579a6c36600d8593a46e
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-12-01 23:16:41 +0800

    Update 杨辉三角的变形.cc

--- Question/杨辉三角的变形.cc
+++ Question/杨辉三角的变形.cc
@@ -53,3 +53,44 @@ int main(){
     
     return 0;
 }
+
+
+//花费更多的空间，但使算法更加精简
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    while(cin >> n){
+        vector<vector<int>> vv(n, vector<int>(2 * n - 1, 0));
+        vv[0][0] = 1;
+        for(int i = 1; i < n; i++){
+            vv[i][0] = vv[i][2 * i] = 1;
+            for(int j = 1; j < 2 * i; j++){
+                //如果为第一列，只需加两个值
+                if(j == 1){
+                    vv[i][j] = vv[i - 1][j] + vv[i - 1][j - 1];
+                }
+                //不为第一列，需要加三个值
+                else{
+                    vv[i][j] = vv[i - 1][j] + vv[i - 1][j - 1] + vv[i - 1][j - 2];
+                }
+            }
+        }
+        
+        int i = 1;
+        for(; i < 2 * n; i++){
+            if(vv[n - 1][i] % 2 == 0){
+                cout << i + 1 << endl;
+                break;
+            }
+        }
+        
+        if(i == 2 * n){
+            cout << -1 << endl;
+        }
+    }
+    
+    return 0;
+}
