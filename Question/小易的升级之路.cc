f24bd51c7dc81c412df7171ebcac19e0ae37e0c3
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-12-02 00:00:32 +0800

    Update 小易的升级之路.cc

--- Question/小易的升级之路.cc
+++ Question/小易的升级之路.cc
@@ -4,30 +4,35 @@ using namespace std;
 
 //公约数
 int Gcd(int a, int b){
-    if(!b){
-        return a;
+    while(a % b){
+        int tmp = b;
+        b = a % b;
+        a = tmp;
     }
-    return Gcd(b, a % b);
+    
+    return b;
 }
 
 int main(){
     int n, a;
     //输入怪兽数量和小易的初始值
     while(cin >> n >> a){
-        vector<int> monster(n);
+        vector<int> monster(n, 0);
         //输入怪兽的防御
-        for(int i = 0; i < monster.size(); i++){
+        for(int i = 0; i < n; i++){
             cin >> monster[i];
         }
+        
         //遍历怪物
         for(int i = 0; i < monster.size(); i++){
-            if(a > monster[i]){
+            if(a >= monster[i]){
                 a += monster[i];
             }
             else {
                 a += Gcd(a, monster[i]);
             }
         }
+        
         //输出
         cout << a << endl;
     }
