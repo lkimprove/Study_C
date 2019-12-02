579405386b904607547ee59cba3edec27a92a6bd
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-12-02 21:15:40 +0800

    Update n个数里出现次数大于等于一半的数.cc

--- Question/n个数里出现次数大于等于一半的数.cc
+++ Question/n个数里出现次数大于等于一半的数.cc
@@ -20,3 +20,75 @@ int main(){
     
     return 0;
 }
+
+//修改版1.0
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> num;
+    int n;
+    while(cin >> n){
+        num.push_back(n);
+    }
+    
+    sort(num.begin(), num.end());
+    
+    cout << num[num.size() / 2 - 1] << endl;
+    
+    return 0;
+}
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> num;
+    int n;
+    while(cin >> n){
+        num.push_back(n);
+    }
+    
+    sort(num.begin(), num.end());
+    
+    cout << num[num.size() / 2 - 1] << endl;
+    
+    return 0;
+}
+
+
+//修改版2.0
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> num;
+    int n;
+    while(cin >> n){
+        num.push_back(n);
+    }
+    
+    int count = 1, ret = 0;
+    for(int i = 0; i < num.size(); i++){
+        if(count == 0){
+            ret = num[i];
+            count++;
+        }
+        else{
+            if(ret == num[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+    }
+    
+    cout << ret << endl;
+    
+    return 0;
+}
