8f4c7a12aaa8a450332cefa6646f7011c26aae3a
lkimprove <47627543+lkimprove@users.noreply.github.com>
2019-12-07 20:21:19 +0800

    Update 不用加减乘除做加法.cc

--- Question/不用加减乘除做加法.cc
+++ Question/不用加减乘除做加法.cc
@@ -1,25 +1,24 @@
-#include <iostream>
-using namespace std;
-
-int Add(int num1, int num2){
-	while (num2 != 0){
-		//本位相加结果
-		int sum = num1 ^ num2;
-		//进位
-		int carry = (num1 & num2) << 1;
-		num1 = sum;
-		num2 = carry;
-	}
-	return num1;
-}
-
-int main(){
-	int num1, num2;
-	//输入
-	while (cin >> num1 >> num2){
-		int ret = Add(num1, num2);
-		//输出
-		cout << ret << endl;
-	}
-	return 0;
-}
+class Solution {
+public:
+    int Add(int num1, int num2)
+    {
+        //^ 求本位
+        //& 求进位
+        //当不存在进位时，运算结束
+        
+        int standard = num1 ^ num2;
+        int carry = num1 & num2;
+        
+        while(carry){
+            carry = carry << 1;
+            
+            int tmp1 = standard ^ carry;
+            int tmp2 = standard & carry;
+            
+            standard = tmp1;
+            carry = tmp2;
+        }
+        
+        return standard;
+    }
+};
