445ce2ab7722ab720f9b3c1a8a7d2fffd692b74e
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-03-04 18:00:40 +0800

    Create 分解因数.cc

---
+++ Question/分解因数.cc
@@ -0,0 +1,41 @@
+// write your code here cpp
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    while(cin >> n){
+        vector<int> ret;
+        int cur = n;
+        
+        for(int i = 2; i <= sqrt(cur); i++){
+            while(1){
+                if(cur % i == 0){
+                    ret.push_back(i);
+                    cur /= i;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        if(cur != 1){
+            ret.push_back(cur);
+        }
+        
+        cout << n << " = ";
+        int size = ret.size();
+        for(auto it : ret){
+            cout << it;
+            if(size != 1){
+                cout << " * ";
+            }
+            size--;
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
