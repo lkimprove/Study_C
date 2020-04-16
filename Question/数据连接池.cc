93d20db93bf4cf1ae1cb6e3ad386677629df2849
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-04-16 11:45:03 +0800

    Update 数据连接池.cc

--- Question/数据连接池.cc
+++ Question/数据连接池.cc
@@ -6,26 +6,24 @@ using namespace std;
 int main(){
     int n;
     while(cin >> n){
-        //创建数据连接池
         set<string> pool;
-        string key, val;
-        
-        //获取最大连接数
         int max = 0;
+        string key, value;
+        
         for(int i = 0; i < n; i++){
-            cin >> key >> val;
-            if(val == "connect"){
+            cin >> key >> value;
+            
+            //是否建立连接
+            if(value == "connect"){
                 pool.insert(key);
             }
-            else if(val == "disconnect"){
+            else if(value == "disconnect"){
                 pool.erase(key);
             }
             
-            //获取当前连接数
-            int size = pool.size();
-            //得出历史最大连接数
-            if(size > max){
-                max = size;
+            //判断当前连接数量
+            if(pool.size() > max){
+                max = pool.size();
             }
         }
         
