bbb17b5eae42765f36fa118a11196d8fe055fddf
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-09-03 16:49:59 +0800

    Update 红与黑.cc

--- Question/红与黑.cc
+++ Question/红与黑.cc
@@ -63,3 +63,62 @@ int main(){
     }
     return 0;
 }
+
+
+
+
+//简化版
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void DFS(int x, int y, int& ret, vector<vector<char>>& map){
+    //防止越界 + 遇到白瓷砖无法向前走的情况
+    if(x < 0 || y < 0 || x >= map.size() || y >= map[0].size() || map[x][y] == '#'){
+        return;
+    }
+    
+    //计数
+    ret++;
+    //避免二次进入同一块黑瓷砖
+    map[x][y] = '#';
+    
+    //遍历上下左右四个方向
+    DFS(x + 1, y, ret, map);
+    DFS(x - 1, y, ret, map);
+    DFS(x, y + 1, ret, map);
+    DFS(x, y - 1, ret, map);
+}
+
+
+int main(){
+    int m, n;
+    while(cin >> m >> n){
+        vector<vector<char>> map(m, vector<char>(n));
+        int ret = 0, x = 0, y = 0;
+        
+        //输入
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                cin >> map[i][j];
+                
+                if(map[i][j] == '@'){
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        
+        DFS(x, y, ret, map);
+        
+        cout << ret << endl;
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
