eaec9bc6650f6dae1afd3cabe3e05c5819ecd0ff
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-11-01 15:51:52 +0800

    Create 反转链表.cc

---
+++ Question/反转链表.cc
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr){
+            return head;
+        }
+
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while(curr != nullptr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+};
