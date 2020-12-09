b98c2e9e98286166f59deb06f6e4784387aeb957
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-09 03:26:14 +0000

    Update 查找组成一个偶数最接近的两个素数.cc

--- Question/查找组成一个偶数最接近的两个素数.cc
+++ Question/查找组成一个偶数最接近的两个素数.cc
@@ -2,11 +2,9 @@
 #include <algorithm>
 using namespace std;
 
-//判断一个数是否为素数
-bool IsPrimeNumber(int x){
-    //只需查到sqrt(x)即可
-    for(int i = 2; i <= sqrt(x); i++){
-        if(x % i == 0){
+bool IsPrime(int number){
+    for(int i = 2; i <= sqrt(number); i++){
+        if(number % i == 0){
             return false;
         }
     }
@@ -16,13 +14,12 @@ bool IsPrimeNumber(int x){
 
 
 int main(){
-    int n;
-    while(cin >> n){
-        //从中间位置开始寻找，遇到的第一对素数即为结果
-        for(int i = n / 2; i > 0; i--){
-            if(IsPrimeNumber(i) && IsPrimeNumber(n - i)){
+    int number;
+    while(cin >> number){
+        for(int i = number / 2; i > 0; i--){
+            if(IsPrime(i) && IsPrime(number - i)){
                 cout << i << endl;
-                cout << n - i << endl;
+                cout << number - i << endl;
                 break;
             }
         }
