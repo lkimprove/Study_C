6991833edb7911770b1f4a2ceb1b0d0fbfc845c5
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-09 03:12:35 +0000

    Update 二进制插入.cc

--- Question/二进制插入.cc
+++ Question/二进制插入.cc
@@ -2,6 +2,24 @@ class BinInsert {
 public:
     int binInsert(int n, int m, int j, int i) {
         // write code here
+        int curr = 0;
+        while(j <= i){
+            int temp = 1 & (m >> curr);
+            n |= (temp << j);
+            
+            j++;
+            curr++;
+        }
+        
+        return n;
+    }
+};
+
+
+class BinInsert {
+public:
+    int binInsert(int n, int m, int j, int i) {
+        // write code here
         
         //将m左移j位，再插入即可
         n = n | (m << j);
