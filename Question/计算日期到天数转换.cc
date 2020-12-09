404d0587378840c13c348b85da48c0553cd5f20d
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-09 15:34:46 +0000

    Update 计算日期到天数转换.cc

--- Question/计算日期到天数转换.cc
+++ Question/计算日期到天数转换.cc
@@ -2,29 +2,33 @@
 #include <vector>
 using namespace std;
 
+bool IsLeapYear(int year){
+    if(year % 4 == 0 && year % 100 != 0){
+        return true;
+    }
+    else if(year % 400 == 0){
+        return true;
+    }
+    
+    return false;
+}
+
+
 int main(){
+    vector<int> monthDay = {0, 31, 28, 31, 30, 31, 30 ,31, 31, 30, 31, 30, 31};
     int year, month, day;
     while(cin >> year >> month >> day){
-        vector<int> monthDay = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        
-        //判断平闰年
-        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
-            monthDay[2] = 29;
-        }
-        
-        //判断输入合法性
-        if(month < 1 || month > 12 || day < 1 || day > monthDay[month]){
-            cout << -1 << endl;
-        }
-        
         int ret = 0;
-        //前几个月的天数
+        
         for(int i = 1; i < month; i++){
+            if(i == 2 && IsLeapYear(year)){
+                ret += 1;
+            }
+            
             ret += monthDay[i];
         }
-        //本月天数
-        ret += day;
         
+        ret += day;
         cout << ret << endl;
     }
     
