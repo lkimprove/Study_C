47ea1fa0e24c58aae4e594988144567758d4ca1f
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-11 15:55:06 +0000

    Update 手套.cc

--- Question/手套.cc
+++ Question/手套.cc
@@ -52,3 +52,26 @@ int main(){
 	}
 	return 0;
 }
+
+
+class Gloves {
+public:
+    int findMinimum(int n, vector<int> left, vector<int> right) {
+        // write code here
+        int sum = 0, left_sum = 0, left_min = INT_MAX, right_sum = 0, right_min = INT_MAX;
+        for(int i = 0; i < n; i++){
+            if(left[i] * right[i] == 0){
+                sum += left[i] + right[i];
+                continue;
+            }
+            
+            left_sum += left[i];
+            right_sum += right[i];
+            
+            left_min = min(left_min, left[i]);
+            right_min = min(right_min, right[i]);
+        }
+        
+        return sum + min(left_sum - left_min + 1, right_sum - right_min + 1) + 1;
+    }
+};
