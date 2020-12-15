4f97f6f7d633810850f5a80ac903ea0e07dedeae
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-15 08:46:48 +0000

    Update 字符串通配符.cc

--- Question/字符串通配符.cc
+++ Question/字符串通配符.cc
@@ -3,19 +3,25 @@
 using namespace std;
 
 bool Match(string& str1, string& str2, int i = 0, int j = 0){
+    //同时走到末尾
     if(str1[i] == '\0' && str2[j] == '\0'){
         return true;
     }
+    //str1/str2提前走到末尾
     else if(str1[i] == '\0' || str2[j] == '\0'){
         return false;
     }
+    //当前位置字符相等/当前位置为'?',向前匹配一个字符
     else if(str1[i] == str2[j] || str1[i] == '?'){
         return Match(str1, str2, i + 1, j + 1);
     }
+    //当前位置为'*'
     else if(str1[i] == '*'){
+        //匹配0个字符/匹配1个字符/匹配若干个字符
         return Match(str1, str2, i + 1, j) || Match(str1, str2, i + 1, j + 1) || Match(str1, str2, i, j + 1);
     }
     
+    //当前位置字符不想等
     return false;
 }
 
