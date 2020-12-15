61d94de6a52357f151d1f3d17e72849bc9b7a42e
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-15 08:23:41 +0000

    Update 字符串通配符.cc

--- Question/字符串通配符.cc
+++ Question/字符串通配符.cc
@@ -2,38 +2,18 @@
 #include <string>
 using namespace std;
 
-bool MatchString(string& s1, string& s2, int i, int j){
-    //同时到结尾，表示已经匹配完
-    if(s1[i] == '\0' && s2[j] == '\0'){
+bool Match(string& str1, string& str2, int i = 0, int j = 0){
+    if(str1[i] == '\0' && str2[j] == '\0'){
         return true;
     }
-    
-    //只有一个到结尾，匹配出错
-    if(s1[i] == '\0' || s2[j] == '\0'){
+    else if(str1[i] == '\0' || str2[j] == '\0'){
         return false;
     }
-    
-    if(s1[i] == s2[j]){
-        return MatchString(s1, s2, i + 1, j + 1);
+    else if(str1[i] == str2[j] || str1[i] == '?'){
+        return Match(str1, str2, i + 1, j + 1);
     }
-    //匹配一个字符
-    else if(s1[i] == '?'){
-        //若对应字符合法，则跳过
-        if((s2[j] >= 'a' && s2[j] <= 'z') || (s2[j] >= 'A' && s2[j] <= 'Z') || (s2[j] >= '0' && s2[j] <= '9')){
-            return MatchString(s1, s2, i + 1, j + 1);
-        }
-        else{
-            return false;
-        }
-    }
-    //匹配0个 || 匹配1个 || 匹配多个
-    else if(s1[i] == '*'){
-        if((s2[j] >= 'a' && s2[j] <= 'z') || (s2[j] >= 'A' && s2[j] <= 'Z') || (s2[j] >= '0' && s2[j] <= '9')){
-            return MatchString(s1, s2, i + 1, j) || MatchString(s1, s2, i + 1, j + 1) || MatchString(s1, s2, i, j + 1);
-        }
-        else{
-            return MatchString(s1, s2, i + 1, j);
-        }
+    else if(str1[i] == '*'){
+        return Match(str1, str2, i + 1, j) || Match(str1, str2, i + 1, j + 1) || Match(str1, str2, i, j + 1);
     }
     
     return false;
@@ -41,13 +21,12 @@ bool MatchString(string& s1, string& s2, int i, int j){
 
 
 int main(){
-    string s1;
-    while(getline(cin, s1)){
-        string s2;
-        getline(cin, s2);
+    string str1;
+    while(getline(cin, str1)){
+        string str2;
+        getline(cin, str2);
         
-        bool ret = MatchString(s1, s2, 0, 0);
-        if(ret){
+        if(Match(str1, str2)){
             cout << "true" << endl;
         }
         else{
