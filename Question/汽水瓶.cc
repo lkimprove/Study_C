a1a6cf73dbfca1e09e2fcc6f85aa1f71b0687155
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-20 06:44:44 +0000

    Update 汽水瓶.cc

--- Question/汽水瓶.cc
+++ Question/汽水瓶.cc
@@ -1,29 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int Bottle(int x){
-	int ret = 0;
-	while (x > 2){
-		//可换的汽水数
-		int cur = x / 3;
-		//剩余的汽水瓶
-		x = x % 3 + cur;
-		ret += cur;
-	}
-	//如果剩余的汽水瓶数为 2，则可以多换一瓶
-	if (x == 2){
-		ret++;
-	}
-	return ret;
-}
-
 int main(){
-	int x;
-  //输入
-	while (cin >> x){
-		int ret = Bottle(x);
-    //输出
-		cout << ret << endl;
-	}
-	return 0;
+    int n;
+    while(cin >> n){
+        if(n == 0){
+            break;
+        }
+        
+        int ret = 0;
+        while(n >= 3){
+            ret += n / 3;
+            n = n % 3 + n / 3;
+        }
+        
+        if(n == 2){
+            ret++;
+        }
+        
+        cout << ret << endl;
+    }
+    
+    return 0;
 }
