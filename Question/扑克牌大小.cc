a0c59a39520a1036b56207d5d043a7ccc799953e
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-20 06:35:16 +0000

    Update 扑克牌大小.cc

--- Question/扑克牌大小.cc
+++ Question/扑克牌大小.cc
@@ -104,56 +104,55 @@ int main(){
 }
 
 
-
+//优化
 #include <iostream>
-#include <string>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 int main(){
     string str;
     while(getline(cin, str)){
-        //双王最大
+        //判断是否有双王
         if(str.find("joker JOKER") != string::npos){
             cout << "joker JOKER" << endl;
+            continue;
+        }
+        
+        //获取两幅手牌
+        size_t pos = str.find('-');
+        string s1 = str.substr(0, pos);
+        string s2 = str.substr(pos + 1);
+        
+        //获取两副牌的数量
+        int size1 = count(s1.begin(), s1.end(), ' ') + 1;
+        int size2 = count(s2.begin(), s2.end(), ' ') + 1;
+        
+        string temp = "3 4 5 6 7 8 9 10 J Q K A 2";
+        //如果牌的数量不相等，判断是否有炸弹
+        if(size1 != size2){
+            if(size1 == 4){
+                cout << s1 << endl;
+            }
+            else if(size2 == 4){
+                cout << s2 << endl;
+            }
+            else{
+                cout << "ERROR" << endl;
+            }
         }
         else{
-            //获取两副手牌
-            size_t npos = str.find('-');
-            string s1 = str.substr(0, npos);
-            string s2 = str.substr(npos + 1);
-            
-            //计算两副手牌的数量
-            int num1 = count(s1.begin(), s1.end(), ' ') + 1;
-            int num2 = count(s2.begin(), s2.end(), ' ') + 1;
-            
-            //获取每副牌的第一张牌
             string first1 = s1.substr(0, s1.find(' '));
             string first2 = s2.substr(0, s2.find(' '));
             
-            string tmp = "3 4 5 6 7 8 9 10 J Q K A 2 joker JOKER";
-            //牌数相同，同类型的牌比大小
-            if(num1 == num2){
-                if(tmp.find(first1) > tmp.find(first2)){
-                    cout << s1 << endl;
-                }
-                else{
-                    cout << s2 << endl;
-                }
+            if(temp.find(first1) < temp.find(first2)){
+                cout << s2 << endl;
             }
-            //牌数不同，只剩炸弹可以比较
             else{
-                if(num1 == 4){
-                    cout << s1 << endl;
-                }
-                else if(num2 == 4){
-                    cout << s2 << endl;
-                }
-                else{
-                    cout << "ERROR" << endl;
-                }
+                cout << s1 << endl;
             }
         }
+        
     }
     
     return 0;
