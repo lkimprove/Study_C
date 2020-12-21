04b48abce506a8d74cd8cb4d7126d0193693ec83
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-12-21 09:24:46 +0000

    Update 查找两个字符串a,b中的最长公共子串.cc

--- Question/查找两个字符串a,b中的最长公共子串.cc
+++ Question/查找两个字符串a,b中的最长公共子串.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
 #include <vector>
+#include <string>
 using namespace std;
 
 int main(){
@@ -13,30 +13,27 @@ int main(){
             swap(str1, str2);
         }
         
-        int count = 0, flag = 0;
+        
         vector<vector<int>> dp(str1.size() + 1, vector<int>(str2.size() + 1, 0));
-        for(int i = 1; i < str1.size(); i++){
-            for(int j = 1; j < str2.size(); j++){
+        
+        int flag = 0, size = 0;
+        for(int i = 1; i <= str1.size(); i++){
+            for(int j = 1; j <= str2.size(); j++){
                 if(str1[i - 1] == str2[j - 1]){
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    
-                    if(count < dp[i][j]){
-                        count = dp[i][j];
-                        flag = i - count;
-                    }
+                }
+                
+                if(size < dp[i][j]){
+                    size = dp[i][j];
+                    flag = i - size;
                 }
             }
         }
         
-        string ret;
-        while(count > 0){
-            ret += str1[flag++];
-            count--;
-        }
+        string ret = str1.substr(flag, size);
         
         cout << ret << endl;
     }
-    
     
     return 0;
 }
